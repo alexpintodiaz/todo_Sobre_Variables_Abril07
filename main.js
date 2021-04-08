eaf2788f6ad5e36a4a5c87927c641fdebc046ad8
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-07 22:28:52 -0500

    1er_taller_04

---
+++ main.js
@@ -0,0 +1,11 @@
+var nombre = "Alexander Pinto";
+var edad = 29;
+var casado = false;
+
+console.log(nombre);
+console.log(edad);
+console.log(casado);
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(casado));
